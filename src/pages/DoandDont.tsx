ddce3b0704f5f6f8e463d46a5c66e8be5c15c11b
Leanna <persaudleanna@gmail.com>
2026-04-19 19:12:50 -0400

    Changed Dos and Dont's page

--- src/pages/DoandDont.tsx
+++ src/pages/DoandDont.tsx
@@ -70,17 +70,19 @@ function Donts(){
 }
 
 export default function DoandDont({contrast, toggleContrast}:{contrast:boolean, toggleContrast:any}){
-    const [stage,setStage] = useState(0)
+    const [stage,setStage] = useState(1)
 
     return (
         <Layout page="Dos and Don'ts" tooltip='Discover what activities you should and should not do at the current stage in your recovery.'
          current={2} contrast={contrast} toggleContrast={toggleContrast}>
             <div className="row text-center m-3">
                 <div className="col">
-                    <button type="button" className="btn btn-success w-100" onClick={()=>setStage(1)}>View Do's</button>
+                    <button type="button" className={`btn ${stage==1 ? "success-clicked" : "btn-success"} w-100`}
+                     onClick={()=>setStage(1)}>View Do's</button>
                 </div>
                 <div className="col">
-                    <button type="button" className="btn btn-danger w-100" onClick={()=>setStage(2)}>View Don'ts</button>
+                    <button type="button" className={`btn ${stage==2 ? "danger-clicked" : "btn-danger"} w-100`}
+                     onClick={()=>setStage(2)}>View Don'ts</button>
                 </div>
             </div>
             {stage==1 && (<Dos/>)}
